4d4b01b030965593ed716ebcf28c21789b1adb79
Valdrin Dauti <vadauti@nbp-68-145.nbp.ruw.rutgers.edu>
2015-11-18 18:08:34 -0500

    changed javascript to add plus delete buttons + rows

--- js/table.js
+++ js/table.js
@@ -1,1 +1,37 @@
-table.js
+var buttons = document.getElementsByClassName('btn btn-danger');
+var addButton = document.getElementsByClassName("btn-primary")[0];
+var userTable = document.getElementsByTagName("table")[0];
+
+addButton.addEventListener("click", addRow);
+
+function removeRow(){
+  this.parentNode.parentNode.remove();
+}
+
+function addRow() {
+  var rowCount = document.getElementsByTagName("tr").length;
+  var newRow = userTable.insertRow(rowCount - 1);
+  var firstNameCell = newRow.insertCell(0);
+  var lastNameCell = newRow.insertCell(1);
+  var emailCell = newRow.insertCell(2);
+  var deleteBtnCell = newRow.insertCell(3);
+
+  var deleteButton = document.createElement("button");
+  deleteButton.appendChild(document.createTextNode("X"));
+  deleteButton.setAttribute("class", "btn btn-danger");
+
+  firstNameCell.innerHTML = document.getElementById("firstName").value;
+  lastNameCell.innerHTML = document.getElementById("lastName").value;
+  emailCell.innerHTML = document.getElementById("email").value;
+  deleteBtnCell.appendChild(deleteButton);
+  deleteButton.addEventListener("click", removeRow);
+
+  document.getElementById("firstName").value = "";
+  document.getElementById("lastName").value = "";
+  document.getElementById("email").value = "";
+
+}
+
+for(i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener("click", removeRow);
+}
